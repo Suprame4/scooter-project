c600510b4416abaedbada80add42183977afb461
Suprame4 <adriancortesgonz@gmail.com>
2023-02-24 11:30:36 -0800

    first commit, the rentScooter method for ScooterApp still needs to be complete and writing jest tests

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,82 @@ const Scooter = require('./Scooter')
 
 class ScooterApp {
   // ScooterApp code here
+
+  constructor(){
+
+    this.stations = {
+      "location1": [],
+      "location2": [],
+      "location3": []
+    }
+    //an object whose keys are usernames to store all users 
+    this.registeredUsers = {
+
+    } 
+
+  }
+
+  registerUser(username, password, age){
+    //check for exceptions first 
+    if(age < 18 ){
+      throw new Error("too young to register")
+    }
+
+    if(this.registeredUsers[username]){
+      throw new Error("already registered")
+    }
+
+    this.registeredUsers[username] = new User(username, password, age)
+    console.log("user has been registered")
+    console.log("test registerUser: ", this.registerUser)
+    console.log("test registerUser: ", this.registeredUsers[username])
+  }
+
+
+  loginUser(username, password){
+    
+    this.registeredUsers[username][username].login(password)
+    if(this.registeredUsers[username][username].loggedin === true){
+      console.log("user has been logged in")  
+    }
+    else{
+      throw new Error("Username or password is incorrect")  
+    }
+
+  }
+
+  logoutUser(username){
+
+    this.registeredUsers[username][username].logout()
+    if( this.registeredUsers[username][username].loggedin ===  false){
+      console.log("user is logged out")
+    }
+
+    if(!this.registeredUsers[username][username]){
+      throw new Error("no such user is logged in")
+    }
+
+  }
+
+  createScooter(station){
+    this.stations[station].push(new Scooter(station))
+    if(this.station[station] == ){
+      console.log("created new scooter")
+    }
+
+  }
+
+  dockScooter(scooter, station){
+
+  }
+
+  renterScooter(scooter, user){
+
+  }
+
+  print(){
+
+  }
 }
 
 module.exports = ScooterApp
--- src/User.js
+++ src/User.js
@@ -1,5 +1,36 @@
 class User {
   // User code here
+
+  constructor(username, password, age){
+    this.username = username
+    this.password = password
+    this.age = age
+
+    this.loggedIn = false 
+  }
+
+  //the user has the following methods (each called by ScooterApp)
+  login(password){
+
+    if(password === this.password){
+      console.log("user logged in")
+      return this.loggedIn = true
+    }
+    else{
+      throw new Error("incorrect password") 
+    }
+
+  }
+
+  logout(){
+    if(this.loggedIn){
+      console.log("user has logged out successfully")
+      return this.loggedIn = false
+    }
+    else{
+      console.log("log out unsuccessful")
+    }
+  }
 }
 
 module.exports = User
